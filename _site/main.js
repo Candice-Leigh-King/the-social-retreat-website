2ca4b1c5609cd3fc35a5f42aae85355e2fd01c94
Candice-Leigh-King <candyschofie@gmail.com>
2021-03-16 12:23:56 +0200

    b

--- _site/main.js
+++ _site/main.js
@@ -28,30 +28,56 @@ $(document).ready(function() {
 				});
 		});
 
-// reviews
+		// faqs
 
-	    $(document).ready(function(){
-	      $('.one-time').slick({
-	        draggable: true,
-	        arrows: true,
-	        adaptiveHeight: true,
-	        autoplaySpeed: 5000
-	      });
-	    });
+		$(".accordion1").click(function(){
 
+			$(this).toggleClass('active');
+			$(".panel1").slideToggle();
+			})
 
-			 // gallery slider
-			 $('.multiple-items').slick({
-	   infinite: true,
-	   slidesToShow: 4,
-	   slidesToScroll: 4
-	 });
+		$(".accordion2").click(function(){
 
-	 $('.two-items').slick({
-	infinite: true,
-	slidesToShow: 2,
-	slidesToScroll: 2
-});
+			$(this).toggleClass('active');
+			$(".panel2").slideToggle();
+			})
+
+		$(".accordion3").click(function(){
+
+			$(this).toggleClass('active');
+			$(".panel3").slideToggle();
+			})
+
+		$(".accordion4").click(function(){
+
+			$(this).toggleClass('active');
+			$(".panel4").slideToggle();
+			})
+
+		$(".accordion5").click(function(){
+
+			$(this).toggleClass('active');
+			$(".panel5").slideToggle();
+			})
+
+		$(".accordion6").click(function(){
+
+			$(this).toggleClass('active');
+			$(".panel6").slideToggle();
+			})
+
+		$(".accordion7").click(function(){
+
+			$(this).toggleClass('active');
+			$(".panel7").slideToggle();
+			})
+
+		$(".accordion8").click(function(){
+
+			$(this).toggleClass('active');
+			$(".panel8").slideToggle();
+			})
+
 
 // epic gallery
 
@@ -72,3 +98,70 @@ $(document).ready(function() {
  });
 
 // end accordian
+
+$(document).ready(function() {
+		$(window).scroll( function(){
+			$('.fadeinone').each( function(i){
+						var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+						var bottom_of_window = $(window).scrollTop() + $(window).height();
+						if( bottom_of_window > bottom_of_element ){
+							$(this).animate({'opacity':'1'},500);
+						}
+				});
+		});
+});
+
+		$(document).ready(function() {
+				$(window).scroll( function(){
+					$('.fadeintwo').each( function(i){
+								var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+								var bottom_of_window = $(window).scrollTop() + $(window).height();
+								if( bottom_of_window > bottom_of_element ){
+									$(this).animate({'opacity':'1'},1000);
+								}
+						});
+				});
+		});
+
+				$(document).ready(function() {
+						$(window).scroll( function(){
+							$('.fadeinthree').each( function(i){
+										var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+										var bottom_of_window = $(window).scrollTop() + $(window).height();
+										if( bottom_of_window > bottom_of_element ){
+											$(this).animate({'opacity':'1'},1500);
+										}
+								});
+						});
+				});
+
+// slide in from right
+
+ $(document).ready(function() {
+		$(window).scroll( function(){
+			$('.slideinright').each( function(i){
+				var bottom_of_element = $(this).offset().top;
+				var bottom_of_window = $(window).scrollTop() + $(window).height();
+				if( bottom_of_window > bottom_of_element ){
+						$(this).animate({'margin-left':'0px'},2500);
+				}
+				});
+			});
+	});
+
+
+// on right end
+
+// slide in from left
+
+$(document).ready(function() {
+$(window).scroll( function(){
+$('.slideinleft').each( function(i){
+var bottom_of_element_left = $(this).offset().top;
+var bottom_of_window = $(window).scrollTop() + $(window).height();
+if( bottom_of_window > bottom_of_element_left ){
+$(this).animate({'margin-left':'0px'},2000);
+}
+});
+});
+});
