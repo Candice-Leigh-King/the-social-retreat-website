d4bef865a2e3aa87bfc65c38c52bad5a631feb1b
Candice-Leigh-King <candyschofie@gmail.com>
2022-03-23 18:03:55 +0700

    c

--- main.js
+++ main.js
@@ -30,6 +30,54 @@ $(document).ready(function() {
 
 		// faqs
 
+		$(".accordion1").click(function(){
+
+			$(this).toggleClass('active');
+			$(".panel1").slideToggle();
+			})
+
+		$(".accordion2").click(function(){
+
+			$(this).toggleClass('active');
+			$(".panel2").slideToggle();
+			})
+
+		$(".accordion3").click(function(){
+
+			$(this).toggleClass('active');
+			$(".panel3").slideToggle();
+			})
+
+		$(".accordion4").click(function(){
+
+			$(this).toggleClass('active');
+			$(".panel4").slideToggle();
+			})
+
+		$(".accordion5").click(function(){
+
+			$(this).toggleClass('active');
+			$(".panel5").slideToggle();
+			})
+
+		$(".accordion6").click(function(){
+
+			$(this).toggleClass('active');
+			$(".panel6").slideToggle();
+			})
+
+		$(".accordion7").click(function(){
+
+			$(this).toggleClass('active');
+			$(".panel7").slideToggle();
+			})
+
+		$(".accordion8").click(function(){
+
+			$(this).toggleClass('active');
+			$(".panel8").slideToggle();
+			})
+
 
 // epic gallery
 
